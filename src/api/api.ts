59321cf61d2650a661804b63dfc4dbc4188e4a02
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-05-27 13:08:10 +0100

    attendee fetch logic

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,5 @@
 import type { Event } from '../types/event.types';
+import type { Attendee } from '../types/attendee.types';
 
 const API_URL = 'https://event-horizon-api.up.railway.app/api';
 
@@ -16,6 +17,17 @@ export async function fetchEventById(id: string): Promise<Event> {
   const response = await fetch(`${API_URL}/events/${id}`);
 
   if (!response.ok) throw new Error(`Failed to fetch event with ID: ${id}`);
+
+  const data = await response.json();
+
+  return data;
+}
+
+export async function fetchAttendeesByEventId(id: string): Promise<Attendee[]> {
+  const response = await fetch(`${API_URL}/events/${id}/attendees`);
+
+  if (!response.ok)
+    throw new Error(`Failed to fetch attendees for event ID: ${id}`);
 
   const data = await response.json();
 
